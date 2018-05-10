7aa7a229385842129782f10d230a8f54eac8a201
David Silva <davidsfernandes2@gmail.com>
2018-05-10 11:18:56 -0300

    Update MainClient.java

--- src/MainClient.java
+++ src/MainClient.java
@@ -2,6 +2,8 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class MainClient {
 	public static void main(String[] args)
@@ -23,10 +25,30 @@ public class MainClient {
 		System.out.print("Insert your address: ");
 		str[1] = scanner.nextLine();
 
-		str[2] = InetAddress.getLocalHost().toString().substring(InetAddress.getLocalHost().toString().indexOf("/") + 1);	
+		String command = null;
+           
+ 		if(System.getProperty("os.name").equals("Linux"))
+                	command = "ifconfig";
+            	else
+                	command = "ipconfig";
+            Runtime r = Runtime.getRuntime();
+            Process p = r.exec(command);
+            Scanner s = new Scanner(p.getInputStream());
+ 
+            StringBuilder sb = new StringBuilder("");
+            while(s.hasNext())
+                sb.append(s.next());
+            String ipconfig = sb.toString();
+            Pattern pt = Pattern.compile("192\\.168\\.[0-9]{1,3}\\.[0-9]{1,3}");
+            Matcher mt = pt.matcher(ipconfig);
+            mt.find();
+            str[2] = mt.group();
+
+		System.out.println(str[2]);	
 
 		new Client(str[0], str[2], 12345, str[1]).execute();
 
 		scanner.close();
 	}
 }
+
