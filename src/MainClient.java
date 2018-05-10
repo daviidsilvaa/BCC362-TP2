6b934e74dec594b9b9a9a4f5e5daf5a0577e02ec
daviidsilvaa <davidsfernandes2@gmail.com>
2018-05-10 13:25:18 -0300

    Corrige merge do MainClient

--- src/MainClient.java
+++ src/MainClient.java
@@ -18,32 +18,9 @@ public class MainClient {
 		// recebe o IP local
 		str[1] = new LocalIP().get();
 
-<<<<<<< HEAD
 		// recebe o IP do server
 		System.out.print("Insert server address: ");
 		str[2] = scanner.nextLine();
-=======
-		String command = null;
-           
- 		if(System.getProperty("os.name").equals("Linux"))
-                	command = "ifconfig";
-            	else
-                	command = "ipconfig";
-            Runtime r = Runtime.getRuntime();
-            Process p = r.exec(command);
-            Scanner s = new Scanner(p.getInputStream());
- 
-            StringBuilder sb = new StringBuilder("");
-            while(s.hasNext())
-                sb.append(s.next());
-            String ipconfig = sb.toString();
-            Pattern pt = Pattern.compile("192\\.168\\.[0-9]{1,3}\\.[0-9]{1,3}");
-            Matcher mt = pt.matcher(ipconfig);
-            mt.find();
-            str[2] = mt.group();
-
-		System.out.println(str[2]);	
->>>>>>> 7aa7a229385842129782f10d230a8f54eac8a201
 
 		// inicializa o Cliente
 		new Client(str[0], str[1], 12345, str[2]).execute();
@@ -51,4 +28,3 @@ public class MainClient {
 		scanner.close();
 	}
 }
-
